94026782c7e538cf1c2648da911fcf9b64c6c702
alice8levi <alice8levi@gmail.com>
2025-01-24 12:49:23 +0300

    start routes

--- public/index.php
+++ public/index.php
@@ -10,26 +10,34 @@ define("VIEWS", APP . '/views');
 
 define("CORE", ROOT . '/core');
 
+define("CONFIG", ROOT . '/config');
+
 define("PUBLIC", ROOT . '/public');
 
 
 
-require CORE . '/functions.php';
-
+require_once CORE . '/functions.php';
+require_once CONFIG . '/routes.php';
+require_once CORE . '/router.php';
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+// dump(trim($_SERVER['REQUEST_URI'], '/'));
+// dump($uri);
+
+// dump($_GET); 
+// dump($_SERVER['QUERY_STRING']); // добавить флаги в hta без пробела
+
 
 if ($uri === '') {
     require CONTROLLERS . '/index.php';
-} elseif ($uri == 'contacts') {
+} else if ($uri == 'contacts') {
     require CONTROLLERS . '/contacts.php';
- } 
+}
+else if 
+($uri == 'post') {
+    dump('SOME POST');
+}
   else {
         abort();
 }
- //elseif ($uri == 'post') {
-//     dd("SHOW POST");
-// } else {
-//    
-// }
 
