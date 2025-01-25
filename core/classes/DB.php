e6acf8445b2d9a4f6e880caaf2ac758f50beebaf
alice8levi <alice8levi@gmail.com>
2025-01-25 09:27:07 +0300

    db singleton

--- core/classes/DB.php
+++ core/classes/DB.php
@@ -1,18 +1,39 @@
 <?php
+//цель синглтона реализовать возможность создания только 1 экземпляра класса
+// через инкапсуляцию конструктора
 class DB
 {
     private $conn;
     private PDOStatement $stmt;
-    public function __construct(array $db_config)
+    private static $instance = null;
+    private function __construct()
+    {
+    }
+    public function getConnection(array $db_config)
     {
         $dsn = "mysql:host={$db_config['host']};dbname={$db_config['dbname']};charset={$db_config['charset']}";
         try {            
             $this->conn = new PDO($dsn, $db_config['username'], $db_config['password'], $db_config['options']);
+            return $this;
         } catch (PDOException $e) {
             // echo "DB Error: {$e->getMessage()}";
             abort(500);
         }
     }
+    private function __clone()
+    {
+    }
+    public function __wakeup()
+    {
+    }
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
     public function query($query, $params = [])
     {
         $this->stmt = $this->conn->prepare($query);
--- public/index.php
+++ public/index.php
@@ -6,13 +6,18 @@ require_once CORE . '/functions.php';
 
 require CORE . '/classes/DB.php';
 $db_config = require CONFIG . '/db.php';
-$db = new DB($db_config);
+// $db = new DB($db_config);
 
 
 // $posts = $db->query("SELECT * FROM posts ORDER BY post_id DESC")->fetchAll();
 // dd($posts);
 
 // dd($db);
+
+/* $db2 = new Db($db_config);
+dump($db);
+dd($db2); */
+$db = (DB::getInstance())->getConnection($db_config);
 
 require_once CORE . '/router.php';
 
